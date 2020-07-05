2f29f1cd89ce42432d40ae8ad228e6fa6edec00a
michaelkim <michael.kim@aurishealth.com>
2020-07-05 01:00:27 -0700

    finished task 1

--- src/chatgui.cpp
+++ src/chatgui.cpp
@@ -118,7 +118,8 @@ ChatBotPanelDialog::ChatBotPanelDialog(wxWindow *parent, wxWindowID id)
     ////
 
     // create chat logic instance
-    _chatLogic = new ChatLogic(); 
+    _chatLogic = std::make_unique<ChatLogic>(); 
+    // _chatLogic = new ChatLogic(); 
 
     // pass pointer to chatbot dialog so answers can be displayed in GUI
     _chatLogic->SetPanelDialogHandle(this);
@@ -135,7 +136,8 @@ ChatBotPanelDialog::~ChatBotPanelDialog()
     //// STUDENT CODE
     ////
 
-    delete _chatLogic;
+    std::cout << "ChatBotPanelDialog Destructor" << std::endl;
+    // delete _chatLogic;
 
     ////
     //// EOF STUDENT CODE
